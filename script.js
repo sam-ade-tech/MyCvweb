ca7967319df5dacd72c32ca719efc34bbd06b470
BlessingAdewale <adelekeblessingadewale@gmail.com>
2026-04-24 07:58:13 +0100

    Add: updated about section and improved UI

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const menuBtn = document.getElementById('menuBtn');
     const navItem = document.getElementById('nav-item');
@@ -6,3 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
       navItem.classList.toggle('active');
     });
   });
+  const bars = document.querySelectorAll(".bar-fill");
+
+  setTimeout(() => {
+    bars.forEach(bar => {
+      bar.style.width = bar.dataset.pct + "%";
+    });
+  }, 200);
